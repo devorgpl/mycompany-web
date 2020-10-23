ead838bed06f87ab86451cb3248a3cfd903fc442
Cyprian <cyprian.sniegota@gino.nl>
2020-10-23 14:29:37 +0200

    lint fix

--- src/app/@section-main/pages/main-pages.component.spec.ts
+++ src/app/@section-main/pages/main-pages.component.spec.ts
@@ -1,16 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { MainPagesComponent } from './main-pages.component';
+import {MainPagesComponent} from './main-pages.component';
 
 describe('PagesComponent', () => {
   let component: MainPagesComponent;
   let fixture: ComponentFixture<MainPagesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainPagesComponent ]
+      declarations: [MainPagesComponent],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
