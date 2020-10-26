c482567a58bf2ddcad70e653be4ab6ff4def5f32
cyprian <csniegota@yameo.pl>
2020-10-26 01:43:21 +0100

    build prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,15 @@
+import { KeycloakConfig } from 'keycloak-js';
+
+const keycloakConfig: KeycloakConfig = {
+  url: '/auth/',
+  realm: 'my-company',
+  clientId: 'website-client',
+};
+
+
 export const environment = {
   production: true,
+  keycloak: keycloakConfig,
   companyServiceBaseUrl: '/companies/api',
   convertServiceBaseUrl: '/converts/api',
 };
